cef6cecfd47edbd38eee5ffcd29c6947fbaeab39
killuari <killuari@protonmail.com>
2024-05-28 01:10:57 +0200

    Add shuffle_answers() method

--- src/question.py
+++ src/question.py
@@ -1,8 +1,15 @@
+import random
 from answer import Answer
 
 class Question():
     def __init__(self, question_text: str, answer_texts: list[str], correct_answer_text: str):
         self.__question_text : str = question_text
         self.__answers: list[Answer]
+
         for answer_text in answer_texts:
             self.__answers.append(Answer(answer_text, answer_text == correct_answer_text))
+        self.shuffle_answers()
+
+    def shuffle_answers(self) -> list[Answer]:
+        random.shuffle(self.__answers)
+    

e155379ed41c660f1b73fcdbebdf14201d5d6e08
killuari <killuari@protonmail.com>
2024-05-27 23:56:01 +0200

    Show that class Lives is working

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 from name import Name
 from score import Score
+from lives import Lives
 
 def main():
     print("Hello, Quiz King!")
 
     # Class Name
+    print("\nClass Name")
     name = Name("Hans")
     print(name)
 
     # Class Score
+    print("\nClass Score")
     score = Score()
     score.add(3)
     print(score.get())
@@ -16,6 +19,17 @@ def main():
     print(score.get())
     score.reset()
     print(score.get())
+
+    # Class Lives
+    print("\nClass Lives")
+    lives = Lives()
+    lives.loose_a_life()
+    print(lives.get())
+    for i in range(5):
+        lives.loose_a_life()
+    print(lives.get())
+    print(lives.is_game_over())
+
 
 if __name__ == "__main__":
     main()

476ac895bd96a3a8ee54faba16bb1a949347043e
Luis Kahles <luis.kahles@hm.edu>
2024-05-28 14:17:33 +0200

    Implement Question Handling for Server and File

--- src/text_based_game.py
+++ src/text_based_game.py
@@ -1,13 +1,30 @@
 from player import Player
+from question import Question
 from questions_from_json_file_factory import QuestionsFromJsonFileFactory
 from questions_from_server_factory import QuestionsFromServerFactory
 
 class TextBasedGame():
     def __init__(self):
-        questions_from_file = QuestionsFromJsonFileFactory("assets/questions.json")
-        questions_from_server = QuestionsFromServerFactory("http://127.0.0.1:5000", "abcd1234")
+        self.questions_from_file = QuestionsFromJsonFileFactory("assets/questions.json")
+        self.questions_from_server = QuestionsFromServerFactory("http://127.0.0.1:5000", "abcd1234")
 
     def run(self):
         name = input("Please, enter your name: ")
         player = Player(name)
         print(f"Welcome {player.name()}!")
+
+        if not player.lives().is_game_over():
+            pass
+
+    def get_question(self, index: int) -> Question:
+        question_from_server = self.questions_from_server.get_question(index)
+        if not question_from_server is None:
+            return question_from_server
+        else:
+            return self.questions_from_file.get_question(index)
+
+    def get_total_number_of_questions(self) -> int:
+        if self.questions_from_server.get_total_number_of_questions() != 0:
+            return self.questions_from_server.get_total_number_of_questions()
+        else:
+            return self.questions_from_file.get_total_number_of_questions()

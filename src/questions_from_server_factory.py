97e90fb5caacf64ca31f43ec4f1411060be8b8b3
Luis Kahles <luis.kahles@hm.edu>
2024-05-28 12:08:25 +0200

    Implement get_question()

--- src/questions_from_server_factory.py
+++ src/questions_from_server_factory.py
@@ -1,4 +1,5 @@
 import requests
+from question import Question
 
 class QuestionsFromServerFactory():
     def __init__(self, base_url: str, api_key: str):
@@ -14,3 +15,12 @@ class QuestionsFromServerFactory():
                 return 0
         return 0
     
+    def get_question(self, index: int) -> Question:
+        for i in range(3):
+            response = requests.get(f"{self.__base_url}/{self.__api_key}/question?index={index}")
+            if response.status_code == 200:
+                question_data = response.json()
+                return Question(question_data['question'], question_data['choices'], question_data['correct'])
+            elif response.status_code == 403:
+                return None
+        return None

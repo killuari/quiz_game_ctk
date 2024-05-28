8fcbfe9f7e320a79bfbedd85cdb4e766da89022b
Luis Kahles <luis.kahles@hm.edu>
2024-05-28 14:53:21 +0200

    Make member variables private

--- src/text_based_game.py
+++ src/text_based_game.py
@@ -5,8 +5,8 @@ from questions_from_server_factory import QuestionsFromServerFactory
 
 class TextBasedGame():
     def __init__(self):
-        self.questions_from_file = QuestionsFromJsonFileFactory("assets/questions.json")
-        self.questions_from_server = QuestionsFromServerFactory("http://127.0.0.1:5000", "abcd1234")
+        self.__questions_from_file = QuestionsFromJsonFileFactory("assets/questions.json")
+        self.__questions_from_server = QuestionsFromServerFactory("http://127.0.0.1:5000", "abcd1234")
 
     def run(self):
         name = input("Please, enter your name: ")
@@ -36,14 +36,14 @@ class TextBasedGame():
 
 
     def get_question(self, index: int) -> Question:
-        question_from_server = self.questions_from_server.get_question(index)
+        question_from_server = self.__questions_from_server.get_question(index)
         if not question_from_server is None:
             return question_from_server
         else:
-            return self.questions_from_file.get_question(index)
+            return self.__questions_from_file.get_question(index)
 
     def get_total_number_of_questions(self) -> int:
-        if self.questions_from_server.get_total_number_of_questions() != 0:
-            return self.questions_from_server.get_total_number_of_questions()
+        if self.__questions_from_server.get_total_number_of_questions() != 0:
+            return self.__questions_from_server.get_total_number_of_questions()
         else:
-            return self.questions_from_file.get_total_number_of_questions()
+            return self.__questions_from_file.get_total_number_of_questions()

0a94d22b7f7edcf8b8565d8675469d561997748b
Luis Kahles <luis.kahles@hm.edu>
2024-05-28 10:50:49 +0200

    Add get_question

--- src/questions_from_json_file_factory.py
+++ src/questions_from_json_file_factory.py
@@ -1,9 +1,11 @@
 import json
+import random
 from question import Question
 
 class QuestionsFromJsonFileFactory():
     def __init__(self, path_to_file: str):
         self.__questions : list[Question] = self.__load_questions(path_to_file)
+        random.shuffle(self.__questions)
 
     def __load_questions(self, path_to_file: str) -> list[Question]:
         questions : list[Question] = []
@@ -14,3 +16,12 @@ class QuestionsFromJsonFileFactory():
                 questions.append(Question(data['question'], data['choices'], data['correct']))
 
         return questions
+    
+    def get_total_number_of_questions(self) -> int:
+        return len(self.__questions)
+    
+    def get_question(self, index: int) -> Question:
+        if index >= 0 and index < self.get_total_number_of_questions():
+            return self.__questions[index]
+        else:
+            return None

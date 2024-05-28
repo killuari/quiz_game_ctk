5cea847ec62d67055da5644c538a09a796e6efd3
Luis Kahles <luis.kahles@hm.edu>
2024-05-28 13:10:35 +0200

    Implement interface QuestionsFactory

--- src/questions_factory.py
+++ src/questions_factory.py
@@ -3,11 +3,12 @@ from question import Question
 
 class QuestionsFactory(ABC):
     @abstractmethod
-    def get_total_number_of_questions() -> int:
+    def get_total_number_of_questions(self) -> int:
         pass
 
     @abstractmethod
-    def get_question(index: int) -> Question:
+    def get_question(self, index: int) -> Question:
         pass
 
-    
+    def _json_to_question(self, json: dict) -> Question:
+        return Question(json['question'], json['choices'], json['correct'])

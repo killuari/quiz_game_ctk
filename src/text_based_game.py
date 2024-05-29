52ddf2b306b798f2185f47709edc44651845a66f
Luis Kahles <luis.kahles@hm.edu>
2024-05-29 10:58:14 +0200

    Fix Game if Server is not running

--- src/text_based_game.py
+++ src/text_based_game.py
@@ -7,6 +7,18 @@ class TextBasedGame():
     def __init__(self):
         self.__questions_from_file = QuestionsFromJsonFileFactory("assets/questions.json")
         self.__questions_from_server = QuestionsFromServerFactory("http://127.0.0.1:5000", "abcd1234")
+
+        # Connect to Server
+        self.__connected_to_server = True
+        try:
+            num = self.__questions_from_server.get_total_number_of_questions()
+        except Exception:
+            self.__connected_to_server = False
+        else:
+            if num == 0:
+                self.__connected_to_server = False
+
+        print(self.__connected_to_server)
         
         name = input("Please, enter your name: ")
         self.__player = Player(name)
@@ -40,14 +52,14 @@ class TextBasedGame():
 
 
     def __get_question(self, index: int) -> Question:
-        question_from_server = self.__questions_from_server.get_question(index)
-        if not question_from_server is None:
-            return question_from_server
-        else:
-            return self.__questions_from_file.get_question(index)
+        if self.__connected_to_server:
+            question_from_server = self.__questions_from_server.get_question(index)
+            if not question_from_server is None:
+                return question_from_server
+        return self.__questions_from_file.get_question(index)
 
     def __get_total_number_of_questions(self) -> int:
-        if self.__questions_from_server.get_total_number_of_questions() != 0:
-            return self.__questions_from_server.get_total_number_of_questions()
-        else:
-            return self.__questions_from_file.get_total_number_of_questions()
+        if self.__connected_to_server:
+            if self.__questions_from_server.get_total_number_of_questions() != 0:
+                return self.__questions_from_server.get_total_number_of_questions()
+        return self.__questions_from_file.get_total_number_of_questions()

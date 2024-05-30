92666aad043fdf45c18b08af5456080ae7bf5c56
killuari <killuari@protonmail.com>
2024-05-31 01:02:06 +0200

    Adjust class menu

--- src/graphics_based_game.py
+++ src/graphics_based_game.py
@@ -1,10 +1,18 @@
 import customtkinter as ctk
+from highscore import Highscore
+from menu import Menu
 
 class GraphicsBasedGame():
     def __init__(self):
         ctk.set_appearance_mode("Dark")
+
         self.__app = ctk.CTk()
         self.__app.title("Quiz King")
+        self.__app.minsize(1280, 720)
+        self.__app.resizable(False, False)
+
+        self.__highscore = Highscore("assets/highscore.json")
+        self.__menu = Menu(self.__app)
 
         # self.__create_widgets()
 
@@ -14,9 +22,10 @@ class GraphicsBasedGame():
         pass
 
     def __get_player_name(self):
-        dialog = ctk.CTkInputDialog(text="Please enter your name", title="Name")
-        return dialog.get_input()  # waits for input
+        dialog = ctk.CTkInputDialog(text="Please enter your name", title="Quiz King")
+        return dialog.get_input()
 
     def run(self):
         name = self.__get_player_name()
+        self.__menu.set_welcome_label(name)
         self.__app.mainloop()
--- src/menu.py
+++ src/menu.py
@@ -1,45 +1,43 @@
-from text_based_game import TextBasedGame
-from player import Player
-from highscore import Highscore
+import customtkinter as ctk
+from PIL import Image
 
 class Menu():
-    def __init__(self):
-        self.__options = ["Play", "View highscore", "Exit game"]
-        self.__highscore = Highscore("assets/highscore.json")
-
-        name = input("Please, enter your name: ")
-        self.__player = Player(name)
-        print(f"Welcome {self.__player.name()}!")
-
-        self.__game = TextBasedGame(self.__player, self.__highscore)
-
-    def __str__(self) -> str:
-        line = "------Quizking------"
-        for idx, option in enumerate(self.__options):
-            line += f"\n{chr(ord('a') + idx)}) {option}"
-        return line
+    def __init__(self, app: ctk.CTk):
+        self.__create_widgets(app)
 
     def __play(self):
-        self.__game.run()
-        self.__player.reset()
+        print("play")
 
     def __view_highscore(self):
-        print(self.__highscore)
+        print("highscore")
 
     def __exit_game(self):
-        quit()
+        print("exit game")
 
-    def __get_input(self):
-        choice = ""
-        while choice not in ['a', 'b', 'c']:
-            choice = input("Please, choose either ['a', 'b', 'c']: ")
-        print()
-        match choice:
-            case 'a': self.__play()
-            case 'b': self.__view_highscore()
-            case 'c': self.__exit_game()
+    def __create_widgets(self, app: ctk.CTk):
+        app.grid_columnconfigure(0, weight=1)
+        app.grid_rowconfigure((0, 1), weight=1)
 
-    def run(self):
-        while True:
-            print(self)
-            self.__get_input()
+        logo = ctk.CTkImage(Image.open("assets/logo.jpeg"), size=(1280, 720))
+        background_label = ctk.CTkLabel(app, text="", image=logo)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        self.__welcome_frame = ctk.CTkFrame(app)
+        self.__main_frame = ctk.CTkFrame(app)
+
+        self.__welcome_frame.grid(row=0, sticky="S")
+        self.__main_frame.grid(row=1, sticky="N")
+
+        self.__welcome_label = ctk.CTkLabel(self.__welcome_frame, width=300, height=40)
+        self.__welcome_label.grid(sticky="EW")
+
+        self.__play_button = ctk.CTkButton(self.__main_frame, text="Play", command=self.__play, width=280, height=56)
+        self.__highscore_button = ctk.CTkButton(self.__main_frame, text="View Highscore", command=self.__view_highscore, width=280, height=56)
+        self.__exit_button = ctk.CTkButton(self.__main_frame, text="Exit Game", command=self.__exit_game, width=280, height=56)
+
+        self.__play_button.grid(column=0, row=0, sticky="NSEW", padx=10, pady=10)
+        self.__highscore_button.grid(column=0, row=1, sticky="NSEW", padx=10, pady=10)
+        self.__exit_button.grid(column=0, row=2, sticky="NSEW", padx=10, pady=10)
+
+    def set_welcome_label(self, name: str):
+        self.__welcome_label.configure(text=f"Welcome {name}!")
